77ab64b2236660050b0e8f0d86b950a760984e54
JoeyGitsIt <josephperri3@gmail.com>
2023-05-16 16:39:30 -0400

    finished notes and called some functions

--- recipeapp/src/com/joey/javalearning/App.java
+++ recipeapp/src/com/joey/javalearning/App.java
@@ -1,5 +1,7 @@
 package com.joey.javalearning;
 
+// import java.lang.Enum;
+
 public class App {
   public static void main(String[] args) throws Exception {
     Ingredient ingredient1 = new Ingredient("flour", 500f);
@@ -12,18 +14,34 @@ public class App {
 
     Recipe recipe = new Recipe("bread", ingredientArray, stepsArray);
 
-    System.out.println(recipe.getRecipeName());
-    System.out.println(java.util.Arrays.toString(recipe.getStepsArray()));
-    System.out.println(recipe.getStepsArray()[1]);
+    // System.out.println(recipe.getRecipeName());
+    // System.out.println(java.util.Arrays.toString(recipe.getStepsArray()));
+    // System.out.println(recipe.getStepsArray()[1]);
 
+    calculateTotalWeight(ingredientArray);
+    allergyWarning(Allergen.WHEAT);
+    calculateTotalCalories(20, 20, 20);
   }
 
-  public static void calculateTotalWeight() {
+  public static void calculateTotalWeight(Ingredient[] ingredientArray) {
+    float totalWeight = 0;
 
+    for (Ingredient ingredient : ingredientArray) {
+      totalWeight += ingredient.weightInGrams;
+    }
+
+    System.out.println(totalWeight + "g is the total weight of all ingredients");
   }
 
-  public static void calculateAverageTotalCalories() {
+  public static int calculateTotalCalories(int carbWeight, int proteinWeight, int fatWeight) {
+    int carbCalsPerGram = 4;
+    int proteinCalPerGram = 4;
+    int fatCalPerGram = 9;
 
+    int totalCals = carbWeight * carbCalsPerGram + proteinWeight * proteinCalPerGram + fatWeight * fatCalPerGram;
+
+    System.out.println(totalCals);
+    return totalCals;
   }
 
   public static void allergyWarning(Allergen allergen) {
